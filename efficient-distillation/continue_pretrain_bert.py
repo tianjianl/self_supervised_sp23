addd513b8933f9eb4e8b1831475ce913f1800cf9
tianjianl <tli104@jhu.edu>
2023-05-01 00:54:23 -0400

    updated continue pretrain bert script

--- efficient-distillation/continue_pretrain_bert.py
+++ efficient-distillation/continue_pretrain_bert.py
@@ -18,7 +18,7 @@ from transformers import BertModel, BertTokenizer, DataCollatorForLanguageModeli
 from init_data import data_to_df 
 from rotk import Regularizer
 
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 
 class BertForMaskedLanguageModeling(nn.Module):
 
@@ -32,7 +32,7 @@ class BertForMaskedLanguageModeling(nn.Module):
         if 'large' in model_name:
             hidden_size = 1024
         self.classifier_t = nn.Linear(hidden_size, num_vocab)
-        self.classifier_s = nn.Linear(hidden_size, num_vocab)
+        #self.classifier_s = nn.Linear(hidden_size, num_vocab)
         self.student_layer = student_layer
     
     def forward(self, input_ids, attention_mask):
@@ -43,17 +43,17 @@ class BertForMaskedLanguageModeling(nn.Module):
 
         hidden_states = output.hidden_states
         
-        assert torch.equal(hidden_states[-1], output.last_hidden_state)
+        #assert torch.equal(hidden_states[-1], output.last_hidden_state)
         
         # hidden_states[layer_num] is a tensor with shape (batch_size, max_length, hidden_size)
         # we project hidden_states[layer_num] to (batch_size, max_length, vocabulary_size)
         # then we only select the masked indices in the second dimension to calculate the loss 
         
         teacher_logits = self.classifier_t(hidden_states[-1])
-        student_logits = self.classifier_s(hidden_states[self.student_layer])
+        #student_logits = self.classifier_s(hidden_states[self.student_layer])
         
         
-        return teacher_logits, student_logits
+        return teacher_logits #student_logits
 
 def get_symm_kl(logits_a, logits_b):
     return (F.kl_div(
@@ -72,42 +72,47 @@ def get_symm_kl(logits_a, logits_b):
             )
         ) / logits_a.size(0)
 
-def preprocess_function(examples, tokenizer):
-    return tokenizer(examples["text"], max_length = 128, truncation=True, padding=True)
 
-def train(epoch, tokenizer, model, loader, optimizer, accelerator):
-    
+def train(epoch, tokenizer, model, loader, optimizer, accelerator, accumulation_steps=1):
     model.train()
     start = time.time()
     loss_fct = nn.CrossEntropyLoss()
     
-
     #initializing parameter importance dictionary
     for iteration, data in enumerate(loader, 0):
         
-        print(data)
-        x = data['source_ids']
-        x_mask = data['source_mask']
-        y = data['label']
-        
-        output, output_student = model(input_ids=x, attention_mask=x_mask)
+        x = data['input_ids']
+        x_mask = data['attention_mask']
+        y = data['labels']
+
+        output = model(input_ids=x, attention_mask=x_mask)
         labels = torch.where(x == tokenizer.mask_token_id, y, -100)
-        loss = loss_fct(output.view(-1, output.size(-1)), labels.view(-1))
-        loss += loss_fct(output_student.view(-1, output_student.size(-1)), labels.view(-1))
+        
+        loss = loss_fct(output.view(-1, output.size(-1)), labels.view(-1)) 
+        #loss += loss_fct(output_student.view(-1, output_student.size(-1)), labels.view(-1))
         
         #if args.use_sd:           
         #    #self-distillation: symmetric kl divergence between teacher and student logits
         #    loss += args.sd_alpha * get_symm_kl(output.view(-1), output_student.view(-1))
-
             
         if iteration%50 == 0:
             wandb.log({"Training Loss": loss.item()})
             print(f'Epoch: {epoch}, Iteration: {iteration}, Loss:  {loss.item()}')
         
-        optimizer.zero_grad()
         accelerator.backward(loss)
-        optimizer.step() # update parameters
+        optimizer.step()
+        optimizer.zero_grad()
         
+        """
+        if iteration + 1 % accumulation_steps == 0:
+            loss = loss / accumulation_steps
+            optimizer.step() # update parameters
+            optimizer.zero_grad()
+            loss = 0 
+        else:
+            continue
+        """
+
     end = time.time()
     print(f'Epoch: {epoch} used {end-start} seconds')
 
@@ -146,7 +151,7 @@ def validate(tokenizer, model, val_loader):
 def main(args):
     
     wandb_name = f"{args.lr}-{args.seed}-{args.student_layer}"
-    wandb.init(project=f"bert-continue-pretrain", entity="dogtooooth", name=wandb_name)
+    wandb.init(project=f"bert-continue-pretrain", entity="dogtooooth", name=wandb_name, settings=wandb.Settings(start_method="fork"))
     accelerator = Accelerator()
     device = accelerator.device
     
@@ -156,6 +161,9 @@ def main(args):
     num_vocab = 30522
     model_name = 'bert-large-uncased'
     model = BertForMaskedLanguageModeling(model_name, num_vocab, args.student_layer)
+    for n, p in model.named_parameters():
+        if 'pooler' in n:
+            p.requires_grad = False
     model.to(device)
     
     tokenizer = BertTokenizer.from_pretrained(model_name)
@@ -165,40 +173,39 @@ def main(args):
     np.random.seed(args.seed) # numpy random seed
     torch.backends.cudnn.deterministic = True
     
-    dataset = load_dataset("bookcorpus")
+    print("Now loading bookcorpus dataset")
+    start = time.time()
+    encoded_dataset = load_from_disk('/scratch4/cs601/tli104/bookcorpus_preprocessed')
+    end = time.time()
+    print(f"Load successful, used {end-start} seconds")
     
-    train_dataset = dataset['train']
-    
-    def preprocess_function(examples):
-        return tokenizer(examples["text"], max_length = 512, truncation=True, padding=True)
-
-    encoded_train_dataset = train_dataset.map(preprocess_function, batched=True)
+    encoded_dataset = encoded_dataset.remove_columns("text")
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=args.lr) 
-    
-    train_loader = DataLoader(encoded_train_dataset, batch_size=32, shuffle=True, collate_fn=data_collator)
-    
 
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=args.lr) 
+    train_loader = DataLoader(encoded_dataset, batch_size=16, shuffle=True, collate_fn=data_collator)
     model, optimizer, train_loader = accelerator.prepare(
         model, optimizer, train_loader
     )
+    
 
     for epoch in range(args.epoch):
         train(epoch, tokenizer, model, train_loader, optimizer, accelerator)
         print(f"teacher val ppl {teacher_valid_ppl} | student val ppl {student_val_ppl}")
-        torch.save(model.state_dict(), f"/bert-checkpoints/bert-large-bookcorpus-{epoch}.pt")
-    model.push_to_hub("bert-large-bookcorpus")
+        torch.save(model.state_dict(), f"/scratch4/cs601/tli104/bert-checkpoints/bert-large-bookcorpus-{epoch}.pt")
+        print(f"saved model at /scratch4/cs601/tli104/bert-checkpoints/bert-large-bookcorpus-{epoch}.pt")
+    #model.push_to_hub("bert-large-bookcorpus")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # basic training arguments
-    parser.add_argument("--epoch", default=2, type=int)
+    parser.add_argument("--epoch", default=5, type=int)
     parser.add_argument("--lr", default=0.00001, type=float)
     parser.add_argument("--seed", default=1104, type=int)
-    
+    parser.add_argument("--accumulation_step", default=1)
     # distillation related arguments
-    parser.add_argument("--student_layer", default=8, type=int)
+    parser.add_argument("--student_layer", default=12, type=int)
     args = parser.parse_args()
 
     main(args)
